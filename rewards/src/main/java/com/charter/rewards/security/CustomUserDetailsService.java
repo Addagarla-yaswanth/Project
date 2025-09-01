bda6acb7dca89c4f284aa4f9570b29fa8f69f820
Addagarla-yaswanth <addagarlayashwanth@gmail.com>
2025-09-01 15:27:07 +0530

    Added CustomUserDetailsService for Spring Security authentication using Customer data

--- rewards/src/main/java/com/charter/rewards/security/CustomUserDetailsService.java
+++ rewards/src/main/java/com/charter/rewards/security/CustomUserDetailsService.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 
 @Service
@@ -30,3 +29,4 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new User(customer.getCustName(), customer.getPhoneNo(), new ArrayList<>());
     }
 }
+
