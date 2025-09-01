4ef6576863bf86a830408e66846447fa7bc0f14e
Addagarla-yaswanth <addagarlayashwanth@gmail.com>
2025-09-01 15:23:42 +0530

    Added ResourceNotFoundException for handling missing resource scenarios

--- rewards/src/main/java/com/charter/rewards/exception/ResourceNotFoundException.java
+++ rewards/src/main/java/com/charter/rewards/exception/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
 package com.charter.rewards.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
 }
