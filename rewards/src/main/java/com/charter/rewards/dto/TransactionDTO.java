7cf6af30dfd30b902e1bcdf303385f65125a7d00
Addagarla-yaswanth <addagarlayashwanth@gmail.com>
2025-09-01 15:16:10 +0530

    Added TransactionDTO with transaction and reward details

--- rewards/src/main/java/com/charter/rewards/dto/TransactionDTO.java
+++ rewards/src/main/java/com/charter/rewards/dto/TransactionDTO.java
@@ -2,10 +2,12 @@ package com.charter.rewards.dto;
 
 import java.time.LocalDate;
 import lombok.Data;
+
 @Data
 public class TransactionDTO {
 	private LocalDate date;
 	private Double amount;
 	private String product;
 	private int rewardPoints;
+
 }
