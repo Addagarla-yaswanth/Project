7809c942fe2c17b96c409b8137c7691f8504e10f
Addagarla-yaswanth <addagarlayashwanth@gmail.com>
2025-09-01 15:32:38 +0530

    Added JwtUtil for generating and validating JWT tokens

--- rewards/src/main/java/com/charter/rewards/util/JwtUtil.java
+++ rewards/src/main/java/com/charter/rewards/util/JwtUtil.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-
 import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
@@ -65,4 +64,5 @@ public class JwtUtil {
         final String username = extractUsername(token);
         return (username.equals(custName) && !isTokenExpired(token));
     }
+
 }
