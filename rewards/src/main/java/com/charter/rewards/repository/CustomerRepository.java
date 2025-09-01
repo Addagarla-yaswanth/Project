856fc00d6698ebb47f41459fd88b450665d339b8
Addagarla-yaswanth <addagarlayashwanth@gmail.com>
2025-09-01 15:25:22 +0530

    Added CustomerRepository for managing Customer entity persistence.

--- rewards/src/main/java/com/charter/rewards/repository/CustomerRepository.java
+++ rewards/src/main/java/com/charter/rewards/repository/CustomerRepository.java
@@ -2,9 +2,10 @@ package com.charter.rewards.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.charter.rewards.entity.Customer;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long>{
 
 }
+
