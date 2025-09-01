5edcd32516f737388db02d495f1e49badcced140
Addagarla-yaswanth <addagarlayashwanth@gmail.com>
2025-09-01 15:16:53 +0530

    Added TransactionRequestDTO for creating customer transactions

--- rewards/src/main/java/com/charter/rewards/dto/TransactionRequestDTO.java
+++ rewards/src/main/java/com/charter/rewards/dto/TransactionRequestDTO.java
@@ -2,10 +2,12 @@ package com.charter.rewards.dto;
 
 import java.time.LocalDate;
 import lombok.Data;
+
 @Data
 public class TransactionRequestDTO {
 	private Long customerId;
 	private LocalDate date;
 	private String product;
 	private Double amount;
+
 }
