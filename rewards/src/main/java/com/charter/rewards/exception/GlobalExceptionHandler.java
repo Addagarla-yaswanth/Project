e67f237a929f2dcb9528d0f9deda1954a1cbdb4b
Addagarla-yaswanth <addagarlayashwanth@gmail.com>
2025-09-01 15:22:50 +0530

    Added GlobalExceptionHandler for centralized exception handling with custom error responses

--- rewards/src/main/java/com/charter/rewards/exception/GlobalExceptionHandler.java
+++ rewards/src/main/java/com/charter/rewards/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,3 +35,4 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
